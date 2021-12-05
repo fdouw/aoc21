2d8ee51fd8f8b1945de647dc640177fc05ec6500
Floris Douw <fdouw@hotmail.com>
2021-12-05 17:19:08 +0100

    Add some performance improvements to the python script for day 5
    Went from >1s to 0.3s on my machine.
    Mainly by reducing (nested) loops and converting Pandas DataFrames to Numpy arrays. It seems DataFrames are slow to index into.

--- python/day05.py
+++ python/day05.py
@@ -5,21 +5,6 @@ import numpy as np
 import pandas as pd
 import re
 
-
-def printgrid(lenx=10, leny=10):
-    # Useful for debugging
-    for y in range(leny):
-        print(
-            str.join(
-                "",
-                (
-                    "." if vents[(x, y)] == 0 else str(vents[(x, y)])
-                    for x in range(lenx)
-                ),
-            )
-        )
-
-
 with open("../input/05") as f:
     rawdata = f.readlines()
     linedata = np.array([int(n) for l in rawdata for n in re.split(r",| -> ", l)])
@@ -29,39 +14,36 @@ with open("../input/05") as f:
 
 # Part 1
 # First register all the vents in a sparse grid
-straights = linedf[
-    np.logical_or(linedf["x1"] == linedf["x2"], linedf["y1"] == linedf["y2"])
-]
+straights = np.array(
+    linedf[np.logical_or(linedf["x1"] == linedf["x2"], linedf["y1"] == linedf["y2"])]
+)
 vents = defaultdict(lambda: 0)
-for _, l in straights.iterrows():
+for x1, y1, x2, y2 in straights:
     # Using min and max here, because sometimes the coordinates are in reverse order
-    for y in range(min(l["y1"], l["y2"]), max(l["y1"], l["y2"]) + 1):
-        for x in range(min(l["x1"], l["x2"]), max(l["x1"], l["x2"]) + 1):
-            vents[(x, y)] += 1
+    if y1 == y2:
+        # horizontal
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            vents[(x, y1)] += 1
+    else:
+        # vertical
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            vents[(x1, y)] += 1
 
 # Then count the places with more than 1 vent
-multicount = 0
-for _, v in vents.items():
-    if v > 1:
-        multicount += 1
+multicount = sum((1 for _, v in vents.items() if v > 1))
 print(f"Part 1: {multicount}")
 
 # Part 2
 # We already have the straights, so only consider diagonals
-diags = linedf[
-    np.logical_and(linedf["x1"] != linedf["x2"], linedf["y1"] != linedf["y2"])
-]
-for _, l in diags.iterrows():
-    dx = -1 if l["x1"] > l["x2"] else 1
-    dy = -1 if l["y1"] > l["y2"] else 1
-    dist = abs(l["x2"] - l["x1"])
+diags = np.array(
+    linedf[np.logical_and(linedf["x1"] != linedf["x2"], linedf["y1"] != linedf["y2"])]
+)
+for x1, y1, x2, y2 in diags:
+    dx = -1 if x1 > x2 else 1
+    dy = -1 if y1 > y2 else 1
+    dist = abs(x2 - x1)
     for i in range(dist + 1):
-        vents[(l["x1"] + i * dx, l["y1"] + i * dy)] += 1
+        vents[(x1 + i * dx, y1 + i * dy)] += 1
 
-# printgrid()
-
-multicount = 0
-for _, v in vents.items():
-    if v > 1:
-        multicount += 1
+multicount = sum((1 for _, v in vents.items() if v > 1))
 print(f"Part 2: {multicount}")
