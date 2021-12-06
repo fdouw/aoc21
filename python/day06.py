efd26e66ecdb770d04a02687dbf12fbd93700ca4
Floris Douw <fdouw@hotmail.com>
2021-12-06 11:45:27 +0100

    Add python solution for day 6

--- python/day06.py
+++ python/day06.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
-with open("../input/06") as f:
-    fish = map(int, f.readline().split(","))
+import numpy as np
 
-# Part 1
-for day in range(80):
-    nextfish = list()
-    for f in fish:
-        if f == 0:
-            nextfish.append(8)
-            nextfish.append(6)
-        else:
-            nextfish.append(f - 1)
-    fish = nextfish
-print(f"Part 1: {len(fish)}")
+
+def fishcount(initial, days):
+    """Returns the total number of fish after <days> days, starting with <initial> population."""
+    fishcounts = np.zeros((9), int)
+    for f in initial:
+        fishcounts[f] += 1
+
+    for _ in range(days):
+        resetcount = fishcounts[0]
+        for i in range(1, 9):
+            fishcounts[i - 1] = fishcounts[i]
+        fishcounts[8] = resetcount
+        fishcounts[6] += resetcount
+    return sum(fishcounts)
+
+
+with open("../input/06") as f:
+    fish = list(map(int, f.readline().split(",")))
+
+print(f"Part 1: {fishcount(fish, 80)}")
+print(f"Part 2: {fishcount(fish, 256)}")
