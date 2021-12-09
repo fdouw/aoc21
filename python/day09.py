e828bf280a1acf76ff328c966d7541171a770ec7
Floris Douw <fdouw@hotmail.com>
2021-12-09 11:32:58 +0100

    Optimise and clean up python code for day 9
    All neighbouring areas of height < 9 connect with eachother, so for the union-find we only need to consider height != 9.
    Union-find only needs one pass, so we can remove the outer loop.
    Lastly, some additional assignments when looking up ids can shorten the look-up chain and improve perfomance.

--- python/day09.py
+++ python/day09.py
@@ -5,13 +5,19 @@ from math import prod
 
 def get_basin_id(index: int):
     while basinmap[index] != index:
-        index = basinmap[index]
+        # Use some additional assignments to shorten the chains as we go, this makes look-ups quicker
+        basinmap[index] = index = basinmap[basinmap[index]]
     return index
+
+
+def connect_basins(a, b):
+    basinmap[get_basin_id(a)] = get_basin_id(b)
 
 
 with open("../input/09") as f:
     heightmap = [[int(c) for c in l.strip()] for l in f.readlines()]
 
+# Part 1
 risklevel = 0
 height = len(heightmap)
 width = len(heightmap[0])
@@ -33,48 +39,22 @@ for y in range(height):
 print(f"Part 1: {risklevel}")
 
 
-# basinmap = [[y * width + x for x in range(width)] for y in range(height)]
+# Part 2
+# Use union-find to find the basins, then sort by size
 basinmap = list(range(width * height))
 
-updated = True
-while updated:
-    updated = False
-    for y in range(height):
-        for x in range(width):
-            current = heightmap[y][x]
-            index = y * width + x
-            if heightmap[y][x] == 9:
-                # Height 9 is not part of a basin
-                continue
-            elif (
-                y > 0
-                and get_basin_id(index) != get_basin_id(index - width)
-                and heightmap[y - 1][x] < current
-            ):
-                basinmap[index] = get_basin_id(index - width)
-                updated = True
-            elif (
-                x + 1 < len(heightmap[y])
-                and get_basin_id(index) != get_basin_id(index + 1)
-                and heightmap[y][x + 1] < current
-            ):
-                basinmap[index] = get_basin_id(index + 1)
-                updated = True
-            elif (
-                y + 1 < len(heightmap)
-                and get_basin_id(index) != get_basin_id(index + width)
-                and heightmap[y + 1][x] < current
-            ):
-                basinmap[index] = get_basin_id(index + width)
-                updated = True
-            elif (
-                x > 0
-                and get_basin_id(index) != get_basin_id(index - 1)
-                and heightmap[y][x - 1] < current
-            ):
-                basinmap[index] = get_basin_id(index - 1)
-                updated = True
-    pass
+for y in range(height):
+    for x in range(width):
+        index = y * width + x
+        if heightmap[y][x] != 9:
+            if y > 0 and heightmap[y - 1][x] != 9:
+                connect_basins(index, index - width)
+            if x + 1 < width and heightmap[y][x + 1] != 9:
+                connect_basins(index, index + 1)
+            if y + 1 < height and heightmap[y + 1][x] != 9:
+                connect_basins(index, index + width)
+            if x > 0 and heightmap[y][x - 1] < 9:
+                connect_basins(index, index - 1)
 
 basinsizes = [0] * len(basinmap)
 for b in basinmap:
