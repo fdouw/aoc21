e5168c2dc11525189facd5570c8c3ed7b293f861
Floris Douw <fdouw@hotmail.com>
2021-12-10 11:34:08 +0100

    Add python solution for part 2 of day 10

--- python/day10.py
+++ python/day10.py
@@ -3,11 +3,12 @@
 with open("../input/10") as f:
     lines = f.readlines()
 
-# Part 1
-score_table = {")": 3, "]": 57, "}": 1197, ">": 25137}
+score_table = {")": 3, "]": 57, "}": 1197, ">": 25137, "(": 1, "[": 2, "{": 3, "<": 4}
 matching_chars = {")": "(", "]": "[", "}": "{", ">": "<"}
 open_chars = matching_chars.values()
-score = 0
+
+score_p1 = 0
+scores_p2 = []
 for line in lines:
     stack = []
     for c in line.strip():
@@ -17,7 +18,19 @@ for line in lines:
             if matching_chars[c] == stack[-1]:
                 stack.pop()
             else:
-                score += score_table[c]
+                score_p1 += score_table[c]
                 break
+    else:
+        # No break out of the loop, so this line must be incomplete
+        score = 0
+        while len(stack) > 0:
+            c = stack.pop()
+            score *= 5
+            score += score_table[c]
+        scores_p2.append(score)
 
-print(f"Part 1: {score}")
+print(f"Part 1: {score_p1}")
+
+ordered = sorted(scores_p2)
+score_p2 = ordered[len(scores_p2) // 2]
+print(f"Part 2: {score_p2}")
