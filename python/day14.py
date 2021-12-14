3c746d0c57aba798aa4c105e18fb93daa1f97a00
Floris Douw <fdouw@hotmail.com>
2021-12-14 13:08:26 +0100

    Rewrite day 14 to solve both parts (python)
    I first did part 1 using brute force, that won't work for part 2.
    Now I'm tracking counts of pairs of molecules instead of generating the entire polymer.

--- python/day14.py
+++ python/day14.py
@@ -4,51 +4,39 @@
 from typing import DefaultDict
 
 
-class Molecule:
-    def __init__(self, name, nxt=None):
-        self.name = name
-        self.next = nxt
-
-    def insert_after(self, new_molecule):
-        new_molecule.next = self.next
-        self.next = new_molecule
-
-    def apply_rule(self, rule):
-        if (
-            self.name == rule[0][0]
-            and self.next != None
-            and self.next.name == rule[0][1]
-        ):
-            self.insert_after(Molecule(rule[1]))
-            return True
-        return False
-
-
 with open("../input/14") as f:
     template = f.readline().strip()
     _ = f.readline()
-    insertion_rules = [l.strip().split(" -> ") for l in f.readlines()]
+    rules = [l.strip().split(" -> ") for l in f.readlines()]
 
+# For each type of pair, remember how many there are: don't track the entire polymer
+# A polymer ABC is AB:1, BC:1. A rule AB->X means AB-=1,AX+=1,and XB+=1.
+pair_counts = DefaultDict(int)
 
-counts = DefaultDict(int)
+molecule_counts = [0] * 26
 
-# Create initial polymer
-polymer = prev = Molecule(template[0])
-counts[template[0]] = 1
-for c in template[1:]:
-    counts[c] += 1
-    prev.next = Molecule(c)
-    prev = prev.next
+# count pairs and molecules in initial template
+for i in range(len(template) - 1):
+    pair_counts[(template[i], template[i + 1])] += 1
+    molecule_counts[ord(template[i]) - 65] += 1
+molecule_counts[ord(template[-1]) - 65] += 1
 
-for step in range(10):
-    molecule = polymer
-    while molecule != None:
-        nxt = molecule.next
-        for rule in insertion_rules:
-            if molecule.apply_rule(rule):
-                counts[rule[1]] += 1
-                break
-        molecule = nxt
+# Step through the rules
+for step in range(40):
+    # Copy the pair`_counts each time, because we don't want to consider newly added molecules yet
+    new_counts = pair_counts.copy()
+    for rule in rules:
+        a, b, c = rule[0][0], rule[0][1], rule[1]
+        occurrences = pair_counts[(a, b)]
+        if occurrences > 0:
+            new_counts[(a, b)] -= occurrences
+            new_counts[(a, c)] += occurrences
+            new_counts[(c, b)] += occurrences
+            molecule_counts[ord(c) - 65] += occurrences
+    pair_counts = new_counts
+    if step == 9:
+        diff = max(molecule_counts) - min(c for c in molecule_counts if c > 0)
+        print(f"Part 1: {diff}")
 
-diff = max(counts.values()) - min(counts.values())
-print(f"Part 1: {diff}")
+diff = max(molecule_counts) - min(c for c in molecule_counts if c > 0)
+print(f"Part 2: {diff}")
