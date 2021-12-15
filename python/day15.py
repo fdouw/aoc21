11780f04eb36b6099975a3b147bf99eba9ada23e
Floris Douw <fdouw@hotmail.com>
2021-12-15 13:01:17 +0100

    Refactor day 15 python

--- python/day15.py
+++ python/day15.py
@@ -1,71 +1,47 @@
 #!/usr/bin/env python
 
+
+def get_distance(cave):
+    deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    width = len(cave[0])
+    height = len(cave)
+    distances = [[100_000_000] * width for _ in range(height)]
+    next_search = {(0, 1), (1, 0)}
+
+    # Don't count the starting position, as we never enter it
+    distances[0][0] = 0
+    while len(next_search) > 0:
+        current_search = next_search
+        next_search = set()
+        for x, y in current_search:
+            updated = False
+            for dx, dy in deltas:
+                if 0 <= x + dx < width and 0 <= y + dy < height:
+                    if distances[y + dy][x + dx] + cave[y][x] < distances[y][x]:
+                        distances[y][x] = distances[y + dy][x + dx] + cave[y][x]
+                        updated = True
+            if updated:
+                for dx, dy in deltas:
+                    if 0 <= x + dx < width and 0 <= y + dy < height:
+                        next_search.add((x + dx, y + dy))
+    return distances[-1][-1]
+
+
 with open("../input/15") as f:
-    cave = [[int(c) for c in l.strip()] for l in f.readlines()]
-
-width = len(cave[0])
-height = len(cave)
-deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-# Part 1
-distances = [[100_000_000] * width for _ in range(height)]
-next_search = {(0, 1), (1, 0)}
-
-# Don't count the starting position, as we never enter it
-distances[0][0] = 0
-while len(next_search) > 0:
-    current_search = next_search
-    next_search = set()
-    for x, y in current_search:
-        updated = False
-        for dx, dy in deltas:
-            if 0 <= x + dx < width and 0 <= y + dy < height:
-                if distances[y + dy][x + dx] + cave[y][x] < distances[y][x]:
-                    distances[y][x] = distances[y + dy][x + dx] + cave[y][x]
-                    updated = True
-        if updated:
-            next_search |= {
-                (x + dx, y + dy)
-                for dx, dy in deltas
-                if 0 <= x + dx < width and 0 <= y + dy < height
-            }
-
-print(f"Part 1: {distances[-1][-1]}")
-
+    data = [[int(c) for c in l.strip()] for l in f.readlines()]
 
 # Generate input for part 2
+width, height = len(data[0]), len(data)
 width2, height2 = width * 5, height * 5
-cave2 = [0] * height2
+data2 = [0] * height2
 for y in range(height2):
-    cave2[y] = [0] * width2
+    data2[y] = [0] * width2
     y1 = y % height
     shift_y = y // height
     for x in range(width2):
         x1 = x % width
         shift_x = x // width
-        cave2[y][x] = (cave[y1][x1] + shift_y + shift_x - 1) % 9 + 1
+        data2[y][x] = (data[y1][x1] + shift_y + shift_x - 1) % 9 + 1
 
-# Part 2
-distances = [[100_000_000] * width2 for _ in range(height2)]
-next_search = {(0, 1), (1, 0)}
-
-# Don't count the starting position, as we never enter it
-distances[0][0] = 0
-while len(next_search) > 0:
-    current_search = next_search
-    next_search = set()
-    for x, y in current_search:
-        updated = False
-        for dx, dy in deltas:
-            if 0 <= x + dx < width2 and 0 <= y + dy < height2:
-                if distances[y + dy][x + dx] + cave2[y][x] < distances[y][x]:
-                    distances[y][x] = distances[y + dy][x + dx] + cave2[y][x]
-                    updated = True
-        if updated:
-            next_search |= {
-                (x + dx, y + dy)
-                for dx, dy in deltas
-                if 0 <= x + dx < width2 and 0 <= y + dy < height2
-            }
-
-print(f"Part 2: {distances[-1][-1]}")
+print(f"Part 1: {get_distance(data)}")
+print(f"Part 2: {get_distance(data2)}")
